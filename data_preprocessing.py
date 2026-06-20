d99b081bb3690309b72bb9be9731f1d30aa7e745
Anna <hihliuk.anna@gmail.com>
2026-06-20 23:35:50 +0200

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,38 +1,18 @@
 # Create a new column that contains True/False
-df['worked_with_python'] = (
-    df['LanguageHaveWorkedWith']
-    .str.contains('Python', na=False)
-)
+df['worked_with_python'] = (df['LanguageHaveWorkedWith'].str.contains('Python', na=False))
 
 # Create a new column that contains True/False
-df['learned_with_online_courses'] = (
-    df['LearnCode']
-    .str.contains('Online Courses', na=False)
-)
+df['learned_with_online_courses'] = (df['LearnCode'].str.contains('Online Courses', na=False))
 
 # Filter respondents who have worked with Python
-python_df = df[
-    (df['worked_with_python'] == True)
-]
+python_df = df[(df['worked_with_python'] == True)]
 
 # Keep only records with salary information
-python_df = python_df[
-    python_df['ConvertedCompYearly'].notna()
-]
+python_df = python_df[python_df['ConvertedCompYearly'].notna()]
 
 # Calculate the third quartile (75th percentile)
-third_quartile_salary = (
-    df['ConvertedCompYearly']
-    .quantile(0.75)
-)
+third_quartile_salary = (df['ConvertedCompYearly'].quantile(0.75))
 
 # Respondents above the 75th percentile
 # who also work remotely
-high_earn_remote = (
-    df[
-        (df['ConvertedCompYearly'] > third_quartile_salary)
-        &
-        (df['RemoteWork'] == 'Remote')
-    ]
-    .reset_index(drop=True)
-)
+high_earn_remote = (df[(df['ConvertedCompYearly'] > third_quartile_salary)&(df['RemoteWork'] == 'Remote')].reset_index(drop=True))
